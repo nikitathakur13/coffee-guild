27322194f63301d68d4d09fdcfbc4ff9abdcc654
Nikita Thakur <nikitathakur@Nikitas-MacBook.local>
2022-02-13 13:16:31 +0530

    responsive fixes

--- js/script.js
+++ js/script.js
@@ -16,3 +16,14 @@ $(document).ready(function() {
   }); 
 });
 
+$(function () {
+  count = 0;
+  wordsArray = ["Fragrant", "Delicious", "flavorful", "amazing"];
+  setInterval(function () {
+    count++;
+    $("#word").fadeOut(400, function () {
+      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+    });
+  }, 2000);
+});
+
